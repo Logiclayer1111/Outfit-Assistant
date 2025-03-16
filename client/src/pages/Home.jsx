5a7d064ce0c85dbfffbe34326fe47bff0ceb9cbb
CharanSuggala26 <suggalasaicharan789@gmail.com>
2025-03-17 01:25:33 +0530

    Added Testimonials

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import WorldMapDemo from '../pages/WorldMapDemo'
 import StickyScrollDemo from '../pages/StickyScrollDemo'
+import AnimatedTestimonialsDemo from '../pages/AnimatedTestimonialsDemo'
+
 const Home = () => {
   return (
     <div>
       <WorldMapDemo />
       <StickyScrollDemo />
-      
-
+      <AnimatedTestimonialsDemo />
     </div>
   )
 }
